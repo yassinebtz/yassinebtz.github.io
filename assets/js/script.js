6df4d0dac2ac20c3dcea6e357290b4a4355d6420
yassine.btz <144150262+yassinebtz@users.noreply.github.com>
2023-09-20 15:55:43 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -202,4 +202,26 @@ showProjects();
 
 
 
+// animation etoile 
+
+function createStars() {
+  const starsContainer = document.querySelector('.stars');
+
+  for (let i = 0; i < 1100; i++) {
+      const star = document.createElement('div');
+      star.className = 'star';
+      star.style.left = Math.random() * 100 + 'vw';
+      star.style.top = Math.random() * 100 + 'vh';
+      star.style.animationDelay = Math.random() * 3 + 's';
+      starsContainer.appendChild(star);
+  }
+}
+
+createStars();
+
+
+
+
+
+
 
